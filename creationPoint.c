a5fbc2226cda3f3ae0af960097a059870b4aebcd
Mandaniaina <mandaniaina.randriambinintsoa@esti.mg>
2019-10-09 15:20:26 +0300

    premier pas

---
+++ creationPoint.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+typedef  struct point point;
+struct point
+{
+     int x;
+     int y;
+
+};
+
+int creationpoint()
+{
+    point p[4];
+    int nb,d;
+    printf("combien de point voulez-vous créer?");
+    scanf("%d",&nb);
+    for(d=0;d<nb;d++)
+    {
+        printf("x(%d), y(%d)=",d, d);
+        scanf("%d %d",&(p[d].x), &(p[d].y));
+    }
+    for(d=0;d<nb;d++)
+    {
+        printf("Point %d : (%d,%d)\n",d, p[d].x,p[d].y);
+    }
+
+    int perimetre;
+    perimetre =((p[0].x - p[0].y)+(p[0].x - p[0].y))*3.14*2;
+
+    printf("perimetre = %d",perimetre);
+
+
+    return 0;
+
+}
